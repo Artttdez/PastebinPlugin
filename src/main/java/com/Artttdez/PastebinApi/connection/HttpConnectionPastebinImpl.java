31c2272eaaa9192e194876bb4e57d67a9f5b89d2
Arseniy <284467@niuitmo.ru>
2021-06-01 00:13:44 +0300

    Realize HttpConnectionPastebinImpl

--- src/main/java/com/Artttdez/PastebinApi/connection/HttpConnectionPastebinImpl.java
+++ src/main/java/com/Artttdez/PastebinApi/connection/HttpConnectionPastebinImpl.java
@@ -1,2 +1,66 @@
-package com.Artttdez.PastebinApi.connection;public class HttpConnectionPastebinImpl {
+package com.Artttdez.PastebinApi.connection;
+
+import com.Artttdez.PastebinApi.response.Response;
+import com.Artttdez.PastebinApi.response.ResponseResult;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+public class HttpConnectionPastebinImpl implements HttpConnectionPastebin {
+    private final static String pastebinApiPost = "https://pastebin.com/api/api_post.php";
+    private final PostParameters postParameters = new PostParameters();
+    public HttpConnectionPastebinImpl() {
+    }
+
+    public void Add(String key, String value) {
+        postParameters.put(key, value);
+    }
+
+    public void Remove(String key) {
+        postParameters.remove(key);
+    }
+
+    private HttpURLConnection openConnection() throws IOException {
+        return (HttpURLConnection) URI
+                .create(pastebinApiPost)
+                .toURL()
+                .openConnection();
+    }
+
+    private void send(OutputStream stream) throws IOException {
+        try (DataOutputStream outputStream = new DataOutputStream(stream)) {
+            outputStream.write(postParameters.toUrl().getBytes(StandardCharsets.UTF_8));
+        }
+    }
+
+    private String getResponse(InputStream stream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder builder = new StringBuilder();
+        for (String line; (line = reader.readLine()) != null; ) {
+            builder.append(line).append('\n');
+        }
+        return builder.toString();
+    }
+
+    private Response returnResponse(String response) {
+        if (response.startsWith("Bad API request")) {
+            return ResponseResult.error(response);
+        }
+        return ResponseResult.success(response);
+    }
+
+    public Response post() {
+        HttpURLConnection connection;
+        try {
+            connection = openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            send(connection.getOutputStream());
+            return returnResponse(getResponse(connection.getInputStream()));
+        } catch (IOException e) {
+            return ResponseResult.error("Unable to connect to Pastebin");
+        }
+    }
 }
--- src/main/java/com/Artttdez/PastebinApi/pastebin/PastebinImpl.java
+++ src/main/java/com/Artttdez/PastebinApi/pastebin/PastebinImpl.java
@@ -1,2 +1,13 @@
-package com.Artttdez.PastebinApi.pastebin;public class PastebinImpl {
+package com.Artttdez.PastebinApi.pastebin;
+
+import com.Artttdez.PastebinApi.paste.Paste;
+import com.Artttdez.PastebinApi.response.Response;
+import com.Artttdez.PastebinApi.response.ResponseResult;
+import org.jetbrains.annotations.NotNull;
+
+public class PastebinImpl implements Pastebin{
+    @Override
+    public @NotNull Response post(Paste paste) {
+        return ResponseResult.success("");
+    }
 }
