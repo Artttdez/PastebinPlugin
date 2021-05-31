d1985fa375f18b9d36f407b1b37eb1589fb70adb
Arseniy <284467@niuitmo.ru>
2021-06-01 01:45:18 +0300

    Add Expire date & Paste format

--- src/main/java/com/Artttdez/PastebinApi/paste/Paste.java
+++ src/main/java/com/Artttdez/PastebinApi/paste/Paste.java
@@ -2,4 +2,7 @@ package com.Artttdez.PastebinApi.paste;
 
 public interface Paste {
     String getPasteCode();
+    String getPasteName();
+    ExpireDate getExpireDate();
+    PasteFormat getPasteFormat();
 }
