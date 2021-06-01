e29f447423dd0804c774b62d1c349fb2363a68da
Artemiy <artemiy.karpov@mail.ru>
2021-06-01 22:41:58 +0300

    gui fix#2

--- src/main/java/com/Artttdez/pastebinsender/gui/SettingsGui.java
+++ src/main/java/com/Artttdez/pastebinsender/gui/SettingsGui.java
@@ -6,11 +6,24 @@ public class SettingsGui {
 
     private JPanel panel;
     private JTextField textField1;
+    private JLabel label1;
+
 
     private void checkJLabel() {
     }
 
+
+    public JPanel getPanel() {
+        return panel;
+    }
+
+    public JTextField getTextField() {
+        return textField1;
+    }
+
     private void createUIComponents() {
-        // TODO: place custom component creation code here
+        panel = new JPanel();
+        textField1 = new JTextField();
+        label1 = new JLabel();
     }
 }
--- src/main/java/com/Artttdez/pastebinsender/storage/SettingConfiguration.java
+++ src/main/java/com/Artttdez/pastebinsender/storage/SettingConfiguration.java
@@ -1,6 +1,6 @@
 package com.Artttdez.pastebinsender.storage;
 
-/*
+
 import com.Artttdez.pastebinsender.gui.SettingsGui;
 import com.intellij.openapi.options.Configurable;
 import org.jetbrains.annotations.Nls;
@@ -10,7 +10,7 @@ import javax.swing.*;
 
 public class SettingConfiguration implements Configurable {
 
-   /* private SettingsGui component;
+    private SettingsGui component;
 
     @Override
     @Nls(capitalization = Nls.Capitalization.Title)
@@ -21,46 +21,30 @@ public class SettingConfiguration implements Configurable {
     @Override
     public @Nullable JComponent createComponent() {
         component = new SettingsGui();
+        System.out.println(component.getTextField());
         return component.getPanel();
     }
 
     @Override
     public boolean isModified() {
-        TBlockSettingsState state = TBlockSettingsState.getInstance();
-        boolean modified = !component.getCamelCaseCheckBox().isSelected() == state.isCamelCase();
-        modified |= !component.getKebabCaseCheckBox().isSelected() == state.isKebabCase();
-        modified |= !component.getSnakeCaseCheckBox().isSelected() == state.isSnakeCase();
-        modified |= !((String) Objects.requireNonNull(component.getLangComboBox().getSelectedItem())).equalsIgnoreCase(state.getDestLang());
-        return modified;
+        SettingState state = SettingState.getInstance();
+        return !(component.getTextField().getText().equals(state.getDevKey()));
     }
 
     @Override
     public void apply() {
-        TBlockSettingsState state = TBlockSettingsState.getInstance();
-        state.setCamelCase(component.getCamelCaseCheckBox().isSelected());
-        state.setKebabCase(component.getKebabCaseCheckBox().isSelected());
-        state.setSnakeCase(component.getSnakeCaseCheckBox().isSelected());
-        state.setDestLang((String) component.getLangComboBox().getSelectedItem());
-        updateConverter(state);
-        updateTranslator(state);
+        SettingState state = SettingState.getInstance();
+        state.setDevKey(component.getTextField().getText());
     }
 
     @Override
     public void reset() {
-        TBlockSettingsState state = TBlockSettingsState.getInstance();
-        component.getCamelCaseCheckBox().setSelected(state.isCamelCase());
-        component.getKebabCaseCheckBox().setSelected(state.isKebabCase());
-        component.getSnakeCaseCheckBox().setSelected(state.isSnakeCase());
-        component.getLangComboBox().setSelectedItem(state.getDestLang());
-        updateConverter(state);
-        updateTranslator(state);
+        SettingState state = SettingState.getInstance();
+        component.getTextField().setText(state.getDevKey());
     }
 
     @Override
     public void disposeUIResources() {
         component = null;
     }
- */
-
 }
-*/
